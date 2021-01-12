8dfaec54918576ae1e32c388004a8858307a16e0
laurachebet1997 <rosechebet19971997@gmail.com>
2021-01-12 19:43:55 +0300

    add alert functions

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function response() {
+    alert("your order will be sent to your location")
+    alert("click the checkout button to exit")
+
+};
+function exit() {
+    alert("click the checkout button to exit")
+};
+
